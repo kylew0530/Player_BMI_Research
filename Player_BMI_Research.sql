2f67da5222835297ce57c2da07b2ced8cad7dcdf
Kyle Anderson <kylewanderson108@gmail.com>
2020-11-07 17:29:03 -0500

    Update Player_BMI_Research.sql

--- Player_BMI_Research.sql
+++ Player_BMI_Research.sql
@@ -1,4 +1,4 @@
-#Please read README for explanation on this code. These tables come from scraped Baseball Savant data and personal sql database from it#
+#Please read README for explanation on this code. These tables come from scraped Baseball Savant data and the personal sql database I dumped it into#
 use baseball;
 
 #Getting Hitter Data#
